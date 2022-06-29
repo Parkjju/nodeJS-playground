223507830cfeec2995284511a6dab4ae3b6dfe29
Parkjju <rudwns3927@gmail.com>
2022-06-30 02:32:04 +0900

    ✨ feat: middleware 실습

--- src/server.js
+++ src/server.js
@@ -6,13 +6,21 @@ const app = express();
 // what is server?
 // computer that turned on all the time
 
+const customMiddleware = (req, res, next) => {
+    console.log(req.protocol);
+    console.log(req.url);
+    console.log('Middle!!');
+    next();
+};
+
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT} 🗼`);
 
 const handleHome = (req, res) => {
+    console.log('Final');
     return res.send('<h1>Hello~</h1>');
 };
-
-app.get('/', handleHome);
+app.get('/me', customMiddleware, handleHome);
+app.get('/', customMiddleware, handleHome);
 
 app.listen(PORT, handleListening);
